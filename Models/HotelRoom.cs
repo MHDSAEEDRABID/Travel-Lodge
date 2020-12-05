4e7a177cfbbd0abd56f082ec6ab50ce174f4b2e2
MHDSAEEDRABID <saeedrubid@gmail.com>
2020-12-05 20:41:44 +0200

    Create Project

---
+++ Models/HotelRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHotel.Models
+{
+    public class HotelRoom
+    {
+        public string id { get; set; }
+        public string maker { get; set; }
+        public string img  { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        
+    }
+}
---
+++ Services/HotelRoomService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using BlazorHotel.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BlazorHotel.Services
+{
+    public class HotelRoomService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public HotelRoomService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        private string RoomsFileName
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.ContentRootPath , "Data" , "products.json");
+            }
+        }
+        public List<HotelRoom> GetRooms()
+        {         
+                using (var jsonFileReader = File.OpenText(RoomsFileName))
+                {
+                    return JsonSerializer.Deserialize<List<HotelRoom>>(jsonFileReader.ReadToEnd(),
+                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                        );
+                }
+        }
+    }
+}
+/*
+ D:\AllProjects\BlazorHotel\BlazorHotel\Data\products.json
+ */
