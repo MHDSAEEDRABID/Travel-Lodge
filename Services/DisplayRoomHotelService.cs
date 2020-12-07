679526bc49173c27cb565b2e763c0428a0ff37fc
MHDSAEEDRABID <saeedrubid@gmail.com>
2020-12-07 15:35:04 +0200

    Add New Service

---
+++ Services/DisplayRoomHotelService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorHotel.Models;
+namespace BlazorHotel.Services
+{
+    public class DisplayRoomHotelService
+    {
+        HotelRoom selectedhotelroom;
+
+        public HotelRoom SelectedHotelRoom { get => selectedhotelroom; set => selectedhotelroom = value; }
+        public string id { get { return selectedhotelroom.id; } }
+        public string maker { get { return selectedhotelroom.maker; } }
+        public string img { get { return selectedhotelroom.img; } }
+        public string title { get { return selectedhotelroom.title; } }
+        public string description { get { return selectedhotelroom.description; } }
+    }
+}
